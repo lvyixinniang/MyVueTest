61515d23f6707bad828e175f5db47e7e6d969537
xyy <3426435900@qq.com>
2024-04-03 23:58:54 +0800

    git test

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <h3>模版语法</h3>
+    <p>{{ msg }}</p>
+    <h2>{{ number + 1 }}</h2>
+    <p>{{ !flag }}</p>
+    <h3>{{ flag ? "YES":"NO" }}</h3>
+    <div>{{ message.split("").reverse().join("") }}</div>
+    <p v-html="trunHref"></p>
+</template>
+
+<script>
+export default{
+    data(){
+        return{
+            msg:"神奇的魔法",
+            number:10,
+            flag:true,
+            message:"大家好",
+            trunHref:"<a href='https://www.bing.com'>必应<a/>"
+        }
+    }
+}
+</script>
